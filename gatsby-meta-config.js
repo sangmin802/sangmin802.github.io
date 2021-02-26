14506801794bc5623f6f39685d75679ce7dc3a31
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-02-26 16:19:34 +0900

    21-02-26/16:19 1. utterances 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'sangmin802/sangmin802.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
