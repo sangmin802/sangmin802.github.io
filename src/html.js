9b6f1e6e4a7514bc5ccf0aebb19e3859af57f5dc
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-03-02 15:33:58 +0900

    21-03-02/15:33 1. ServerLess 포스트

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
+          <meta name="google-site-verification" content="WTLVnbAuz9H8tQTTXv_40kS5jXU8MrrROhe5VK2lEos" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
