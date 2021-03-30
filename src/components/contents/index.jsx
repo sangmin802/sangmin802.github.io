20a3a892b47eaabd9c1033744275d0bb66a07a9b
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-03-30 18:10:39 +0900

    21-03-30/18:10 1. 프리티어

--- src/components/contents/index.jsx
+++ src/components/contents/index.jsx
@@ -3,12 +3,29 @@ import { ThumbnailContainer } from '../thumbnail-container'
 import { ThumbnailItem } from '../thumbnail-item'
 import { CATEGORY_TYPE, TAG_TYPE } from '../../constants'
 
-export const Contents = ({ posts, countOfInitialPost, count, category, tag }) => {
+export const Contents = ({
+  posts,
+  countOfInitialPost,
+  count,
+  category,
+  tag,
+}) => {
   const refinedPosts = useMemo(() => {
-    if (tag === TAG_TYPE.ALL) return posts.filter(({ node }) => category === CATEGORY_TYPE.ALL || node.frontmatter.category === category).slice(0, count * countOfInitialPost);
-    return posts.filter(({ node }) => node.frontmatter.tag === tag && node.frontmatter.category === category).slice(0, count * countOfInitialPost);
-  }, [category, tag]);
-
+    if (tag === TAG_TYPE.ALL)
+      return posts
+        .filter(
+          ({ node }) =>
+            category === CATEGORY_TYPE.ALL ||
+            node.frontmatter.category === category
+        )
+        .slice(0, count * countOfInitialPost)
+    return posts
+      .filter(
+        ({ node }) =>
+          node.frontmatter.tag === tag && node.frontmatter.category === category
+      )
+      .slice(0, count * countOfInitialPost)
+  }, [category, tag])
   return (
     <ThumbnailContainer>
       {refinedPosts.map(({ node }, index) => (
